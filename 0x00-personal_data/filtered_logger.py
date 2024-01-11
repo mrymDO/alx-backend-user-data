cd01d532698eee92535a4d2e7a2b287a13bd5b0b
Meryeme <dobli.meryeme@gmail.com>
2024-01-11 03:51:01 -0800

    task2

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -6,6 +6,8 @@ import re
 from typing import List
 import logging
 
+PII_FIELDS = ["name", "email", "phone", "ssn", "password"]
+
 
 class RedactingFormatter(logging.Formatter):
     """ Redacting Formatter class
@@ -43,3 +45,16 @@ def filter_datum(
             message,
         )
     return message
+
+
+def get_logger() -> logging.Logger:
+    """returns a logger"""
+    logger = logging.getLogger("user_data")
+    logger.setLevel(logging.INFO)
+    logger.propagate = False
+
+    handler = logging.streamHandler()
+    handler.setFormatter(fields=PII_FIELDS)
+    logger.addHandler(handler)
+
+    return logger
