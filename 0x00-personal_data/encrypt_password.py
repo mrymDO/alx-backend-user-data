a33ccc90ea95e9026114026a09331fdd329da883
Meryeme <dobli.meryeme@gmail.com>
2024-01-11 05:04:50 -0800

    task6

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -10,6 +10,7 @@ def hash_password(password: str) -> bytes:
     hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
     return hashed_password
 
+
 def is_valid(hashed_password: bytes, password: str) -> bool:
     """"check valid passwword"""
     return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
