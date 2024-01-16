a691fd6e0aaddb304437bdbdd0ed1848cbc5cff7
Meryeme <dobli.meryeme@gmail.com>
2024-01-16 02:20:44 -0800

    task4

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -12,7 +12,12 @@ class Auth:
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """ Check if authentication is required
         """
-        return False
+        if path is None or excluded_paths is None or not excluded_paths:
+            return True
+        for excluded_path in excluded_paths:
+            if path.rstrip('/') == excluded_path.rstrip('/'):
+                return False
+        return True
 
     def authorization_header(self, request=None) -> str:
         """ Get the authorization header from the request
