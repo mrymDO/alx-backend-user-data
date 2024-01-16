9cb0832eb10552f3207647a30027924b5b36843a
Meryeme <dobli.meryeme@gmail.com>
2024-01-16 03:32:34 -0800

    task6

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -19,6 +19,9 @@ auth_type = os.getenv("AUTH_TYPE", 'auth')
 if auth_type == "basic_auth":
     from api.v1.auth.basic_auth import BasicAuth
     auth = BasicAuth()
+else:
+    from api.v1.auth.auth import Auth
+    auth = Auth()
 
 
 @app.errorhandler(404)
