712139f86e9ce8a850f1415502c7d21a8d6eeec1
Meryeme <dobli.meryeme@gmail.com>
2024-01-16 08:41:31 -0800

    task8

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -22,3 +22,18 @@ class BasicAuth(Auth):
 
         base64_part = authorization_header[len("Basic "):].strip()
         return base64_part
+
+
+    def decode_base64_authorization_header(
+            self, base64_authorization_header: str) -> str:
+        """ Decode Base64 Authorization Header
+        """
+        if base64_authorization_header is None or\
+                not isinstance(base64_authorization_header, str):
+                    return None
+        try:
+            decoded_bytes = base64.b64decode(base64_authorization_header)
+            decoded_string = decoded_bytes.decode('utf-8')
+            return decoded_string
+        except Exception as e:
+            return None
