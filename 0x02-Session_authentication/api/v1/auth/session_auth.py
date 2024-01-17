1a1474c45b830e2d3f47582800f2721c9a7cdc48
Meryeme <dobli.meryeme@gmail.com>
2024-01-17 12:49:51 -0800

    task1

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+""" Session authentication
+"""
+from api.v1.auth.auth import Auth
+
+
+class SessionAuth(Auth):
+    """ SessionAuth class for a new authentication mechanism
+    """
+    pass
