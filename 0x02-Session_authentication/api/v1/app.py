11c092a6266a0fca8d8a89334fb76e12d59ee7a8
Meryeme <dobli.meryeme@gmail.com>
2024-01-18 01:07:02 -0800

    fix pycodestyle

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -62,7 +62,7 @@ def before_request():
 
         if auth.require_auth(request.path, excluded_paths):
             if auth.authorization_header(request) is None and\
-                auth.session_cookie(request) is None:
+                    auth.session_cookie(request) is None:
                 abort(401)
             if auth.current_user(request) is None:
                 abort(403)
