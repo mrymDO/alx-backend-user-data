4a9bb2974f4d744a55e8a98beae27244efb279cb
Meryeme <dobli.meryeme@gmail.com>
2024-01-24 16:00:26 -0800

    task7

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -2,10 +2,13 @@
 """Basic Flask app
 """
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from auth import Auth
 
 
 app = Flask(__name__)
+
+AUTH = Auth()
 
 
 @app.route("/")
@@ -13,6 +16,20 @@ def welcome():
     """return a JSON payload
     """
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route("/users", methods=["POST"])
+def register_user():
+    """register user
+    """
+    try:
+        email = request.form.get("email")
+        password = request.form.get("password")
+
+        user = AUTH.register_user(email, password)
+        return jsonify({"email": user.email, "message": "user created"}), 200
+    except ValueError as e:
+        return jsonify({"message": str(e)}), 400
 
 
 if __name__ == "__main__":
