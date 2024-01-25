1a1c43d7a7026c971e840588939c0c67fca5cd4a
Meryeme <dobli.meryeme@gmail.com>
2024-01-25 15:31:21 -0800

    task19

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -83,5 +83,21 @@ def get_reset_password_token() -> str:
         return abort(403, str(e))
 
 
+@app.route("/reset_password", methods=["PUT"])
+def update_password() -> str:
+    """update password
+    """
+    try:
+        email = request.form.get('email')
+        reset_token = request.form.get('reset_token')
+        new_password = request.form.get('new_password')
+
+        AUTH.update_password(reset_token, new_password)
+
+        return jsonify({"email": email, "message": "Password updated"}), 200
+    except ValueError:
+        abort(403)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
