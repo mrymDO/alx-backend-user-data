a6804a6d811ffa19455e9d6c2013cb2d97a05a40
Meryeme <dobli.meryeme@gmail.com>
2024-01-24 16:24:37 -0800

    task10

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -51,3 +51,15 @@ class Auth:
                     password.encode('utf-8'), user.hashed_password)
         except NoResultFound:
             return False
+
+    def create_session(self, email: str) -> str:
+        """ Create a session for the user and return the session ID
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+            session_id = _generate_uuid()
+            user.session_id = session_id
+            self._db._session.commit()
+            return session_id
+        except NoResultFound:
+            return None
