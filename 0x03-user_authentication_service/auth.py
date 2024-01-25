3845461f33a15ff5c50b86a406bcf2337761a19e
Meryeme <dobli.meryeme@gmail.com>
2024-01-24 16:11:00 -0800

    task9

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -17,6 +17,11 @@ def _hash_password(password: str) -> bytes:
 
     return hashed_password
 
+ def _generate_uuid() -> str:
+    """Generate a new UUID and return its string representation
+    """
+    return str(uuid.uuid4())
+
 
 class Auth:
     """Auth class to interact with the authentication database.
@@ -45,8 +50,3 @@ class Auth:
                     password.encode('utf-8'), user.hashed_password)
         except NoResultFound:
             return False
-
-    def _generate_uuid() -> str:
-    """Generate a new UUID and return its string representation
-    """
-    return str(uuid.uuid4())
